077c8b0e826d17e81404d883466f0e1cde3c8d94
Andyou <andyou@animagram.jp>
2026-02-24 08:07:02 +0900

    del unused mod

--- src/common.rs
+++ src/common.rs
@@ -1,16 +1,13 @@
 pub mod bit;
 pub mod pool;
 pub mod parser;
-pub mod manifest;
-pub mod bi_map;
-pub mod index;
 pub mod dot_map_accessor;
 pub mod dot_string;
 pub mod placeholder;
 pub mod log_format;
 
-pub use bi_map::BiMap;
-pub use index::Index;
+pub mod manifest;
+
 pub use dot_map_accessor::DotMapAccessor;
 pub use dot_string::DotString;
 pub use placeholder::Placeholder;
